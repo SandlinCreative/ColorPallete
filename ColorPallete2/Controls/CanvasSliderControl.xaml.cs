3fcd26668285a5b35f7750522c7915830e04ba44
Jeremy Sandlin <sandlincreative@gmail.com>
2021-10-07 07:36:11 -0600

    .

--- ColorPallete2/Controls/CanvasSliderControl.xaml.cs
+++ ColorPallete2/Controls/CanvasSliderControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -7,8 +9,10 @@ namespace ColorPallete2
     /// <summary>
     /// Interaction logic for CanvasSliderControl.xaml
     /// </summary>
-    public partial class CanvasSliderControl : UserControl
+    public partial class CanvasSliderControl : UserControl 
     {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
         public CanvasSliderControl()
         {
             InitializeComponent();
@@ -16,38 +20,40 @@ namespace ColorPallete2
 
         public static int PuckSize = 20;
 
+        private double _xValue=0;
+        private double _yValue=0;
+
         public double XValue {
-            get { return XValue; }
+            get { return Canvas.GetLeft(this.dragObject); ; }
 
-            set
-            {
-                XValue = value;
-
+            set { _xValue = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("XValue"));
             }
         }
         public double YValue {
-            get { return YValue; }
+            get { return Canvas.GetTop(this.dragObject); }
 
-            set
-            {
-                YValue = value;
+            set{ _yValue = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("YValue"));
             }
         }
-        public int MaxValue { get; set; } = -1;
+        //public double MaxValue { get; set; } = -1;
 
         public static DependencyProperty XValueProp = DependencyProperty.Register("XValue", typeof(double), typeof(CanvasSliderControl));
         public static DependencyProperty YValueProp = DependencyProperty.Register("YValue", typeof(double), typeof(CanvasSliderControl));
-        public static DependencyProperty MaxValueProp = DependencyProperty.Register("MaxValue", typeof(int), typeof(CanvasSliderControl));
+        //public static DependencyProperty MaxValueProp = DependencyProperty.Register("MaxValue", typeof(int), typeof(double));
+        public static DependencyProperty PositionProp = DependencyProperty.Register("MaxValue", typeof(int), typeof(Point));
 
 
         UIElement dragObject = null;
-        Point offset;
+        private Point offset;
+        public Point Position { get { return offset; } set { offset = value; } }
 
 
         private void DragPuck_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
             this.dragObject = sender as UIElement;
-            this.offset = e.GetPosition(this.TheCanvas);
+            this.Position = e.GetPosition(this.TheCanvas);
             this.offset.Y -= Canvas.GetTop(this.dragObject);
             this.offset.X -= Canvas.GetLeft(this.dragObject);
             bool captureSuccess = this.TheCanvas.CaptureMouse();
@@ -58,15 +64,16 @@ namespace ColorPallete2
             if (this.dragObject == null)
                 return;
             var position = e.GetPosition(sender as IInputElement);
-            Canvas.SetTop(this.dragObject, position.Y - this.offset.Y);
-            Canvas.SetLeft(this.dragObject, position.X - this.offset.X);
+            Canvas.SetTop(this.dragObject, position.Y - this.Position.Y);
+            Canvas.SetLeft(this.dragObject, position.X - this.Position.X);
             CheckBounds();
         }
 
         private void TheCanvas_PreviewMouseUp(object sender, MouseButtonEventArgs e)
         {
             //CheckBounds();
-            UpdateXY();
+            //UpdateXY();
+            Console.WriteLine($"I was placed here: {Canvas.GetLeft(this.dragObject)},{Canvas.GetTop(dragObject)}");
             this.dragObject = null;
             this.TheCanvas.ReleaseMouseCapture();
         }
@@ -105,16 +112,30 @@ namespace ColorPallete2
 
         private void UpdateXY()
         {
-            if (MaxValue < 0)
-            {
-                XValue = offset.X;
-                YValue = offset.Y;
-            }
-            else
-            {
-                XValue = offset.X * MaxValue / TheCanvas.ActualWidth;
-                YValue = offset.Y * MaxValue / TheCanvas.ActualWidth;
-            }
+            //if (MaxValue < 0)
+            //{
+            //    XValue = Position.X;
+            //    YValue = Position.Y;
+            //}
+            //else
+            //{
+            //    XValue = Position.X * MaxValue / TheCanvas.ActualWidth;
+            //    YValue = Position.Y * MaxValue / TheCanvas.ActualWidth;
+            //}
+        }
+
+        private void DragPuck_ManipulationCompleted(object sender, ManipulationCompletedEventArgs e)
+        {
+        }
+
+        private void DragPuck_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            //Console.WriteLine("I was manipulated");
+        }
+        //private int i = 0;
+        private void DragPuck_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            //Console.WriteLine($"I am moved {i++}");
         }
     }
 }
